0aa5cd5e253d9286ad1020c4b96dfec40f29ff33
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2019-08-09 09:45:08 +0100

    init

---
+++ app/Http/Livewire/DisappearingValue.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DisappearingValue extends Component
+{
+    public $foo;
+    public $bar;
+    public $saved;
+
+    public function mount()
+    {
+        $this->foo = 'foo';
+        $this->bar = 'bar';
+    }
+
+    public function updated($name, $value)
+    {
+        // dd($name);
+        $this->saved = $name;
+    }
+
+    public function render()
+    {
+        return view('livewire.disappearing-value');
+    }
+}
