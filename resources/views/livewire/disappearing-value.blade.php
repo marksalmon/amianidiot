8a77f10264da363e305302f4f547f1e449f9fea6
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2019-08-09 10:08:59 +0100

    Whoops

--- resources/views/livewire/disappearing-value.blade.php
+++ resources/views/livewire/disappearing-value.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <textarea wire:model.lazy.dirty="foo" class="m-4 border rounded {{ $saved === 'bar' ? 'border-2 border-red-500' : 'border-teal-500' }}" rows="3"></textarea>
+    <textarea wire:model.lazy.dirty="foo" class="m-4 border rounded {{ $saved === 'foo' ? 'border-2 border-red-500' : 'border-teal-500' }}" rows="3"></textarea>
     <textarea wire:model.lazy.dirty="bar" class="m-4 border rounded {{ $saved === 'bar' ? 'border-2 border-red-500' : 'border-teal-500' }}"  rows="3"></textarea>
 </div>
